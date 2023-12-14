6648077226b411155bb8c43435f7c323667f3d1c
anushka-u <anushkau032@gmail.com>
2023-12-14 18:53:06 +0530

    DataJPA app

--- 09-DataJPASpringBoot_app/src/main/java/com/jpa/springboot/Application.java
+++ 09-DataJPASpringBoot_app/src/main/java/com/jpa/springboot/Application.java
@@ -1,13 +1,20 @@
 package com.jpa.springboot;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import com.jpa.springboot.repository.ProductRepository;
+import com.jpa.springboot.service.ProductService;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		ConfigurableApplicationContext context =SpringApplication.run(Application.class, args);
+		ProductService productServicve = context.getBean(ProductService.class);
+		
 	}
 
 }
