a2bbf47165689f88e829b8f270d892e106612cb8
anushka-u <anushkau032@gmail.com>
2023-12-14 22:44:34 +0530

    09-updated

--- 09-DataJPASpringBoot_app/src/main/java/com/jpa/springboot/Application.java
+++ 09-DataJPASpringBoot_app/src/main/java/com/jpa/springboot/Application.java
@@ -1,20 +1,13 @@
 package com.jpa.springboot;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-
-import com.jpa.springboot.repository.ProductRepository;
-import com.jpa.springboot.service.ProductService;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		ConfigurableApplicationContext context =SpringApplication.run(Application.class, args);
-		ProductService productServicve = context.getBean(ProductService.class);
-		
+		SpringApplication.run(Application.class, args);
 	}
 
 }
--- 09-DataJPASpringBoot_app/src/main/java/com/jpa/springboot/entity/Product.java
+++ 09-DataJPASpringBoot_app/src/main/java/com/jpa/springboot/entity/Product.java
@@ -7,8 +7,10 @@ import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 @Data
+@RequiredArgsConstructor
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
