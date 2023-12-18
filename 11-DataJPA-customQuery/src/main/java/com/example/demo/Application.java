202143c5c38478df45d3f842ae8cdcfd568ad207
anushka-u <anushkau032@gmail.com>
2023-12-18 17:53:51 +0530

    Custom Queries in Springboot

--- 11-DataJPA-customQuery/src/main/java/com/example/demo/Application.java
+++ 11-DataJPA-customQuery/src/main/java/com/example/demo/Application.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import java.util.Arrays;
+import java.util.List;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,14 +18,28 @@ public class Application {
 		ConfigurableApplicationContext  context = SpringApplication.run(Application.class, args);
 		StudentRepository repository = context.getBean(StudentRepository.class);
 		
-		Student s1 = new Student(1,"Aashish","cse",21);
-		Student s2 = new Student(2,"Rupa","Ec",20);
-		Student s3 = new Student(3,"Kusum","ME",19);
-		Student s4 = new Student(4,"karuna","cse",22);
+//		Student s1 = new Student(1,"Aashish","cse",21);
+//		Student s2 = new Student(2,"Rupa","Ec",20);
+//		Student s3 = new Student(3,"Kusum","ME",19);
+//		Student s4 = new Student(4,"karuna","cse",22);
+//		
+//		repository.saveAll(Arrays.asList(s1,s2,s3,s4));
 		
-		repository.saveAll(Arrays.asList(s1,s2,s3,s4));
+		//calling HQL method to retrieve all record
+		List<Student> students = repository.getAllStudentHql();
+		students.forEach(student -> System.out.println(student));
 		
+		System.out.println("___________________________________________________________");
+		// calling Native-sql query method
+		 students = repository.getAllStudentSql();
+		students.forEach(student -> System.out.println(student));
 		
+		System.out.println("______________________________________________________________");
+		students =repository.getAllStudentByCourse("cse");
+		students.forEach(student -> System.out.println(student));
+		
+		students = repository.getAllStudentByCourseAndAge("cse", 21);
+		students.forEach(student -> System.out.println(student));
 	}
 
 }
--- 11-DataJPA-customQuery/src/main/java/com/example/demo/StudentRepository.java
+++ 11-DataJPA-customQuery/src/main/java/com/example/demo/StudentRepository.java
@@ -1,8 +1,22 @@
 package com.example.demo;
 
-import org.springframework.data.repository.CrudRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+ 
 
 public interface StudentRepository extends CrudRepository<Student, Integer> {
 
+	@Query(value = "From Student " )
+	public List<Student> getAllStudentHql();
+	
+	@Query(value = "select *from student", nativeQuery = true)
+	public List<Student> getAllStudentSql();
+	
+	@Query(value = "From Student where course=:str")
+	public List<Student> getAllStudentByCourse(String str);
+	
+	@Query(value ="From Student where course=:cname and age=:age")
+	public List<Student> getAllStudentByCourseAndAge(String cname, Integer age);
 }
