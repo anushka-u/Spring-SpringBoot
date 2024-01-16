5b1007a2e698ff7e051c8af01691799391e8b0bd
anushka-u <anushkau032@gmail.com>
2024-01-16 21:43:28 +0530

    Cobverting JSOn file into Java object using JAckson

---
+++ 28-JSON-JACKSON-api/src/main/java/in/springboot/restapi/json/java/JsonToJavaConvertor.java
@@ -0,0 +1,26 @@
+package in.springboot.restapi.json.java;
+
+import java.io.File;
+import java.io.IOException;
+
+import com.fasterxml.jackson.core.exc.StreamReadException;
+import com.fasterxml.jackson.databind.DatabindException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import in.springboot.restapi.json.Book;
+
+public class JsonToJavaConvertor {
+
+	public static void main(String[] args) throws StreamReadException, DatabindException, IOException {
+		// TODO Auto-generated method stub
+
+		File jsonFile = new File("book.json");
+		
+		ObjectMapper mapper = new ObjectMapper();
+		
+		Book book = mapper.readValue(jsonFile, Book.class);
+		
+		System.out.println(book);
+	}
+
+}
