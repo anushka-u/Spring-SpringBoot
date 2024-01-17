8b0dbbb1467b81dbad6bd85153b66dce097234c5
anushka-u <anushkau032@gmail.com>
2024-01-17 19:26:10 +0530

    First REST API App

--- 30-restFULservicesAPI/src/main/java/com/spring/CourseRestController.java
+++ 30-restFULservicesAPI/src/main/java/com/spring/CourseRestController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CourseRestController {
 
+	//Working with 2 QueryParams
 	@GetMapping("/course")
 	public String getCourseFee(String cName, String tName)
 	{
